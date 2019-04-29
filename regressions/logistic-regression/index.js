39725c121a3b36ecd10934c657e5732a33d85610
tetsu <tetsuromori@gmail.com>
2019-04-29 19:02:19 +0900

    add logic-regression

---
+++ regressions/logistic-regression/index.js
@@ -0,0 +1,27 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
+
+const {features, labels, testFeatures, testLabels} = loadCSV('../data/cars.csv', {
+    dataColumns: ['horsepower', 'displacement', 'weight'],
+    labelColumns: ['passedemissions'],
+    shuffle: true,
+    splitTest: 50,
+    converters: {
+        passedemissions: (value) => {
+            return value === 'TRUE' ? 1 : 0;
+        }
+    }
+});
+
+const regression = new LogisticRegression(features, labels, {
+    learningRate: 0.5,
+    iteration: 100,
+    batchSize: 50
+});
+
+regression.train();
+// regression.predict([[130, 307, 1.75]]).print();
+// regression.predict([[88, 97, 1.065]]).print();
+console.log( regression.test(testFeatures, testLabels) );
---
+++ regressions/logistic-regression/logistic-regression.js
@@ -0,0 +1,123 @@
+const tf = require('@tensorflow/tfjs');
+const _ = require('lodash');
+
+class LogisticRegression {
+    constructor(features, labels, options) {
+        this.features = this.processFeatures(features);
+        this.labels = tf.tensor(labels);
+        this.mseHistory = [];
+
+        this.options = Object.assign(
+            { learningRate: 0.1, iterations: 1000 },
+            options)
+        ;
+
+        this.weights = tf.zeros([this.features.shape[1], 1]);
+    }
+
+    gradientDescent(features, labels) {
+        const currentGuesses = features.matMul(this.weights).sigmoid();
+        const differences = currentGuesses.sub(labels);
+
+        const slopes = features
+            .transpose()
+            .matMul(differences)
+            .div(features.shape[0]);
+        
+        this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    }
+
+    train() {
+        const {batchSize, iterations} = this.options;
+        const batchQuantity = Math.floor(
+            this.features.shape[0] / batchSize
+        );
+        
+        for (let i=0; i<iterations; i++) {
+            for (let j = 0; j < batchQuantity; j++) {
+                const startIndex = j * batchSize;
+
+                const featuresSlice = this.features.slice(
+                    [startIndex, 0],
+                    [batchSize, -1]
+                )
+                const labelsSlice = this.labels.slice(
+                    [startIndex, 0],
+                    [batchSize, -1]
+                )
+    
+                this.gradientDescent(featuresSlice, labelsSlice);
+            }
+
+            this.recordMSE();
+            this.updateLearningRate();
+        }
+    }
+
+    predict(observations) {
+        return this.processFeatures(observations)
+            .matMul(this.weights)
+            .sigmoid();
+    }
+
+    test(testFeatures, testLabels) {
+        const predictions = this.predict(testFeatures);
+        testLabels = tf.tensor(testLabels);
+    
+        const incorrect = predictions
+          .sub(testLabels)
+          .abs()
+          .sum()
+          .get();
+    
+        return (predictions.shape[0] - incorrect) / predictions.shape[0];
+    }
+
+    processFeatures(features) {
+        features = tf.tensor(features);
+        features = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+        if (this.mean && this.variance) {
+            features = features.sub(this.mean).div(this.variance.pow(0.5));
+        } else {
+            features = this.standardize(features);
+        }
+        
+        return features;
+    }
+
+    standardize(features) {
+        const { mean, variance } = tf.moments(features, 0);
+
+        this.mean = mean;
+        this.variance = variance;
+
+        return features.sub(mean).div(variance.pow(0.5));
+    }
+
+    recordMSE() {
+        const mse = this.features
+            .matMul(this.weights)
+            .sub(this.labels)
+            .pow(2)
+            .sum()
+            .div(this.features.shape[0])
+            .get();
+        
+        this.mseHistory.unshift(mse);
+    }
+
+    updateLearningRate() {
+        if (this.mseHistory.length < 2) {
+            return;
+        }
+
+        if (this.mseHistory[0] > this.mseHistory[1]) {
+            this.options.learningRate /= 2;
+        } else {
+            this.options.learningRate *= 1.05;
+        }
+    }
+}
+
+module.exports = LogisticRegression;
